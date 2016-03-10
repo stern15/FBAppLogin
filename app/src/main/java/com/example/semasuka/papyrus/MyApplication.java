51bfa86ed87ec3613ac70dbf8d4f4bf19c2c7411
stern15 <ssemasuka@gmail.com>
2016-03-11 00:25:20 +0300

    hashtagkey added

--- app/src/main/java/com/example/semasuka/papyrus/MyApplication.java
+++ app/src/main/java/com/example/semasuka/papyrus/MyApplication.java
@@ -1,6 +1,14 @@
 package com.example.semasuka.papyrus;
 
 import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by semasuka on 11/03/2016.
@@ -9,8 +17,26 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        printHashkey();
     }
-    public void printHashkey(){
-        
+
+    public void printHashkey() {
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.semasuka.papyrus",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("COM", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+
+        }
     }
+
 }
