f03cbb4cdf43a2a9850083299a0b2cb6544e29ac
stern15 <ssemasuka@gmail.com>
2016-03-10 23:43:55 +0300

    first commit

---
+++ app/src/main/java/com/example/semasuka/papyrus/MainFragment.java
@@ -0,0 +1,31 @@
+package com.example.semasuka.papyrus;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.FacebookSdk;
+
+/**
+ * Created by semasuka on 10/03/2016.
+ */
+public class MainFragment extends Fragment {
+
+    public MainFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main,container,false);
+    }
+}
