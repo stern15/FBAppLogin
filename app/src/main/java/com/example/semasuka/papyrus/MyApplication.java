dfe608dc4bd2667ea825f5b33eb8293e1fb4b9ad
stern15 <ssemasuka@gmail.com>
2016-03-11 00:08:03 +0300

    adding the hashtagkey method

---
+++ app/src/main/java/com/example/semasuka/papyrus/MyApplication.java
@@ -0,0 +1,16 @@
+package com.example.semasuka.papyrus;
+
+import android.app.Application;
+
+/**
+ * Created by semasuka on 11/03/2016.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+    public void printHashkey(){
+        
+    }
+}
