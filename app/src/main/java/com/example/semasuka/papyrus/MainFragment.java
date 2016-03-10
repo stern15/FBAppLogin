7e17ad054eefc853e45eb5877ac9843f081a1183
stern15 <ssemasuka@gmail.com>
2016-03-11 02:49:36 +0300

    adding the link to facebook

--- app/src/main/java/com/example/semasuka/papyrus/MainFragment.java
+++ app/src/main/java/com/example/semasuka/papyrus/MainFragment.java
@@ -21,11 +21,15 @@ public class MainFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext() );
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main,container,false);
+
+        
+
+
     }
 }
